19f717615335489dac49497cd20ff53f6e9b8f69
jawhien <jawhien@yandex.by>
2021-01-04 01:06:27 +0300

    Added build vars.

--- buildVars.py
+++ buildVars.py
@@ -16,31 +16,32 @@ def _(arg):
 # Add-on information variables
 addon_info = {
 	# add-on Name/identifier, internal for NVDA
-	"addon_name": "addonTemplate",
+	"addon_name": "extendedTotalCMD",
 	# Add-on summary, usually the user visible name of the addon.
 	# Translators: Summary for this add-on
 	# to be shown on installation and add-on information found in Add-ons Manager.
-	"addon_summary": _("Add-on user visible name"),
+	"addon_summary": _("Total Commander"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description": _("""Description for the add-on.
-It can span multiple lines."""),
+	"addon_description": _("This add-on improves the accessability of the Total Commander file manager, mostly versions 8.0 and higher."),
 	# version
-	"addon_version": "x.y",
+	"addon_version": "2.4",
 	# Author(s)
-	"addon_author": "name <name@domain.com>",
+	"addon_author": "Eugene Poplavsky <jawhien@gmail.com>",
 	# URL for the add-on documentation support
-	"addon_url": None,
+	"addon_url": "https://jnsoft.ru/en/articles/nvda/extendedTotalCmd/",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": None,
+	"addon_minimumNVDAVersion": "2017.3",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": None,
+	"addon_lastTestedNVDAVersion": "2020.4",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
 	"addon_updateChannel": None,
+	# file name after build
+	"addon_buildFileName": "total-commander",
 }
 
 # Define the python files that are the sources of your add-on.
@@ -51,14 +52,14 @@ It can span multiple lines."""),
 # pythonSources = ["addon/globalPlugins/*.py"]
 # For more information on SCons Glob expressions please take a look at:
 # https://scons.org/doc/production/HTML/scons-user/apd.html
-pythonSources = []
+pythonSources = ["addon/appModules/*.py", "addon/globalPlugins/extendedTotalCMD/*.py", "addon/installTasks.py"]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]
 
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
-excludedFiles = []
+excludedFiles = ["doc\\en\\readme.md", "doc\\ru\\readme.md"]
 
 # Base language for the NVDA add-on
 # If your add-on is written in a language other than english, modify this variable.
