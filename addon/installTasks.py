124433ea9c769e0f84f48674747864470a733df8
jawhien <jawhien@yandex.by>
2021-03-29 15:21:09 +0300

    It is necessary to check the version of NVDA before installation, due to the changed requirements for compatibility.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -9,8 +9,11 @@ import wx
 import gui
 import addonHandler
 from languageHandler import getLanguage
+from versionInfo import version_year, version_major
 
 addonHandler.initTranslation()
+year = 2018
+major = 3
 
 lang = getLanguage().split("_")[0]
 if lang != "en" and lang != "ru": lang = "en"
@@ -18,6 +21,10 @@ if lang != "en" and lang != "ru": lang = "en"
 donations_url = "https://jnsoft.ru/{lang}/articles/nvda/extendedTotalCmd/donation.php".format(lang=lang)
 
 def onInstall():
+	if (version_year, version_major) < (year, major):
+		gui.messageBox(_("This version of NVDA is incompatible. To install the add-on, NVDA version {year}.{major} or higher is required. Please update NVDA or download an older version of the add-on here: \n{link}").format(year=year, major=major, link="https://github.com/jawhien/extendedTotalCmd/releases/tag/2.5"), _("Error"), style=wx.OK | wx.ICON_ERROR)
+		raise RuntimeError("Incompatible NVDA version")
+
 	manifest = addonHandler.getCodeAddon().manifest
 	# Translators: The text of the dialog shown during add-on installation.
 	message = _(""" {name} - this free add-on for NVDA.
