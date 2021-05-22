00897fe2a1ca7d225dcd823d5a0ccf3616c8abc2
jawhien <jawhien@yandex.by>
2021-05-22 17:18:14 +0300

    Easier update.
    Now the add-on should update automatically after the user confirms the update, and there is no need to download and install it manually.

--- addon/globalPlugins/extendedTotalCMD/updater.py
+++ addon/globalPlugins/extendedTotalCMD/updater.py
@@ -26,7 +26,7 @@ try:
 except NameError:
 	opener = urllib.request.build_opener()
 
-updateInfo = {"text":"", "currentVersion":manifest["version"], "newVersion":"", "getAddonUrl":"", "isAvailable":False}
+updateInfo = {"text":"", "currentVersion":manifest["version"], "newVersion":"", "getAddonUrl":"", "downloadUrl":"", "size":"", "FileName":"", "isAvailable":False}
 
 def loadUpdateInfo():
 	global updateInfo
@@ -48,12 +48,42 @@ def loadUpdateInfo():
 		lang = getLanguage().split("_")[0]
 		if lang != "en" and lang != "ru": lang = "en"
 		updateInfo["getAddonUrl"] = "https://jnsoft.ru/{locale}/articles/nvda/extendedTotalCmd/".format(locale=lang)
+		for asset in latest["assets"]:
+			if asset["content_type"] == "application/x-nvda-addon":
+				updateInfo["downloadUrl"] = asset["browser_download_url"]
+				updateInfo["size"] = asset["size"]
+				updateInfo["FileName"] = asset["name"]
+				break
+
 		updateInfo["isAvailable"] = True
 	else:
 		updateInfo["text"] = _("No update available.")
 
 def getAddon():
-	webbrowser.open(updateInfo["getAddonUrl"])
+	import tempfile, os
+	with opener.open(updateInfo["downloadUrl"]) as addon:
+		content = addon.read()
+	if updateInfo["size"] != len(content):
+		gui.messageBox(_("Data load error"), _("Error"), style=wx.OK|wx.ICON_ERROR)
+		return
+	fd,path = tempfile.mkstemp(".nvda-addon", "tcmd_addon_update-")
+	with open(path, "wb") as file:
+		file.write(content)
+	import core
+	bundle = addonHandler.AddonBundle(path)
+	if not addonHandler.addonVersionCheck.isAddonCompatible(bundle):
+		gui.messageBox(_("This version of NVDA is incompatible. To install the add-on, NVDA version {year}.{major} or higher is required. Please update NVDA or download an older version of the add-on here: \n{link}").format(year=bundle.minimumNVDAVersion[0], major=bundle.minimumNVDAVersion[1], link="https://github.com/jawhien/extendedTotalCmd/releases/tag/2.5"), _("Error"), style=wx.OK|wx.ICON_ERROR)
+		os.close(fd)
+		os.unlink(path)
+		return
+	gui.ExecAndPump(addonHandler.installAddonBundle, bundle)
+	for addon in addonHandler.getAvailableAddons():
+		if not addon.isPendingRemove and bundle.name.lower()==addon.manifest['name'].lower():
+			addon.requestRemove()
+			break
+	os.close(fd)
+	os.unlink(path)
+	if gui.messageBox(_("Changes were made to add-ons. You must restart NVDA for these changes to take effect. Would you like to restart now?"), _("Restart NVDA"), style=wx.YES|wx.NO|wx.ICON_WARNING) == wx.YES: core.restart()
 
 def onCheckForUpdates(event):
 	t = threading.Thread(target=update)
