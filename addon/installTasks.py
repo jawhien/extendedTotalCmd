9e5c2173ce480f36ee09349c073ab471780be5ac
jawhien <jawhien@yandex.by>
2021-05-29 16:08:29 +0300

    Checking before installing the minimal version from the manifest.
    This is a stub for nvda versions that do not have a compatibility check when installing the add-on. Verification must be carried out according to the manifest.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -12,8 +12,6 @@ from languageHandler import getLanguage
 from versionInfo import version_year, version_major
 
 addonHandler.initTranslation()
-year = 2018
-major = 3
 
 lang = getLanguage().split("_")[0]
 if lang != "en" and lang != "ru": lang = "en"
@@ -21,15 +19,22 @@ if lang != "en" and lang != "ru": lang = "en"
 donations_url = "https://jnsoft.ru/{lang}/articles/nvda/extendedTotalCmd/donation.php".format(lang=lang)
 
 def onInstall():
+	manifest = addonHandler.getCodeAddon().manifest
+	if isinstance(manifest["minimumNVDAVersion"], unicode):
+		minVersion = manifest["minimumNVDAVersion"].split(".")
+	else:
+		minVersion = manifest["minimumNVDAVersion"]
+	year = int(minVersion[0])
+	major = int(minVersion[1])
+
 	if (version_year, version_major) < (year, major):
 		gui.messageBox(_("This version of NVDA is incompatible. To install the add-on, NVDA version {year}.{major} or higher is required. Please update NVDA or download an older version of the add-on here: \n{link}").format(year=year, major=major, link="https://github.com/jawhien/extendedTotalCmd/releases/tag/2.5"), _("Error"), style=wx.OK | wx.ICON_ERROR)
 		raise RuntimeError("Incompatible NVDA version")
 
-	manifest = addonHandler.getCodeAddon().manifest
 	# Translators: The text of the dialog shown during add-on installation.
 	message = _(""" {name} - this free add-on for NVDA.
 You can make a donation to the author to help further development of this add-on and other free software.
 You want to make a donation now? For transaction you will be redirected to the website of the developer.""").format(name=manifest['summary'])
 
-	if gui.messageBox(message, caption=_("Request donations for {name}").format(name=manifest['summary']), style=wx.YES_NO|wx.ICON_QUESTION) == wx.YES:
+	if gui.messageBox(message, _("Request donations for {name}").format(name=manifest['summary']), style=wx.YES_NO|wx.ICON_QUESTION) == wx.YES:
 		webbrowser.open(donations_url)
